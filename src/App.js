87c0a839430507b5b4f874cfc53355f2a5bc7aaa
codydeatherage <cdeatherage14@gmail.com>
2020-11-26 18:35:34 -0800

    all equipment add ed to redux store

--- src/App.js
+++ src/App.js
@@ -28,22 +28,121 @@ class App extends Component {
 
     this.setState({ postId: this.state.postId + 1 })
   }
-
+  dispatcher(slot, item){
+    this.props.dispatch({
+      type: `ADD_${slot}`,
+      payload: {
+        name: item,
+        stats: item,
+        slot: item
+      }
+    });
+  }
+//store.getState().allWeapons[store.getState().allWeapons.findIndex(i=>i.name === 'Elder maul')].stats
   async componentDidMount() {
     console.log('mounted');
     let pageNumber = 1;
-    const MAX_PAGES = 35;
-    for(let i = pageNumber; i <= MAX_PAGES;i++){
-      await fetch(`https://api.osrsbox.com/weapons?page=${i}`)
+    const eMax_Pages = 146;
+    for(let i = pageNumber; i <= eMax_Pages; i++){
+      await fetch(`https://api.osrsbox.com/equipment?page=${i}`)
         .then(response => response.json())
         .then(json => {
-          console.log('meta info', json._meta);
-          console.log('"next" field', json._links.next);
-          for (let i = 0; i < json._items.length; i++) {
-            this.props.dispatch({
-              type: 'ADD_WEAPON',
-              payload: { name: json._items[i].name, stats: json._items[i].equipment }
-            })
+          for (let j = 0; j < json._items.length; j++) {
+            const {name, equipment} = json._items[j];
+            switch(json._items[j].equipment.slot){
+              case '2h':
+              case 'weapon': this.props.dispatch({
+                              type: 'ADD_WEAPON',
+                              payload: {
+                                name: name,
+                                stats: equipment,
+                                slot: equipment.slot
+                              }
+                            });
+                            break;
+              case 'head': this.props.dispatch({
+                            type: 'ADD_HELM',
+                            payload: {
+                              name: json._items[j].name,
+                              stats: json._items[j].equipment
+                            }
+                          });
+                          break;
+              case 'cape' : this.props.dispatch({
+                              type: 'ADD_CAPE',
+                              payload: {
+                                name: json._items[j].name,
+                                stats: json._items[j].equipment
+                              }
+                            });
+                            break;
+              case 'body' : this.props.dispatch({
+                              type: 'ADD_BODY',
+                              payload: {
+                                name: json._items[j].name,
+                                stats: json._items[j].equipment
+                              }
+                            });
+                            break;
+              case 'legs' : this.props.dispatch({
+                              type: 'ADD_LEGS',
+                              payload: {
+                                name: json._items[j].name,
+                                stats: json._items[j].equipment
+                              }
+                            });
+                            break;
+              case 'hands' : this.props.dispatch({
+                              type: 'ADD_HANDS',
+                              payload: {
+                                name: json._items[j].name,
+                                stats: json._items[j].equipment
+                              }
+                            });
+                            break;
+              case 'feet' : this.props.dispatch({
+                              type: 'ADD_BOOTS',
+                              payload: {
+                                name: json._items[j].name,
+                                stats: json._items[j].equipment
+                              }
+                            });
+                            break;
+              case 'shield' : this.props.dispatch({
+                                type: 'ADD_OFFHAND',
+                                payload: {
+                                  name: json._items[j].name,
+                                  stats: json._items[j].equipment
+                                }
+                              });
+                              break;   
+              case 'neck' : this.props.dispatch({
+                              type: 'ADD_NECK',
+                              payload: {
+                                name: json._items[j].name,
+                                stats: json._items[j].equipment
+                              }
+                            });
+                            break;
+              case 'ring' : this.props.dispatch({
+                              type: 'ADD_RING',
+                              payload: {
+                                name: json._items[j].name,
+                                stats: json._items[j].equipment
+                              }
+                            })
+                            break;
+              case 'ammo' : this.props.dispatch({
+                              type: 'ADD_AMMO',
+                              payload: {
+                                name: json._items[j].name,
+                                stats: json._items[j].equipment
+                              }
+                            })
+                            break;                                
+              default: console.log('NO MATCH FOUND', json._items[j].equipment.slot); break;   
+            }
+
           }
         })
     }
--- src/Store.js
+++ src/Store.js
@@ -2,30 +2,75 @@ import { createStore } from 'redux'
 
 const initialState = {
   posts: [{id: 1, title: 'Test Post'}],
-  allWeapons: [{name: '', stats:{str: 1, atk: 1}}],
+  allWeapons: [{name: '', stats:{str: 1, atk: 1}, slot:'weapon'}],
+  allHelms: [{name: '', stats:{def: 1}}],
+  allNecks: [{name: '', stats:{neck: 1}}],
+  allCapes: [{name: '', stats:{cape: 1}}],
+  allAmmo: [{name: '', stats:{ammo: 1}}],
+  allBodies: [{name: '', stats:{body: 1}}],
+  allOffhands: [{name: '', stats:{offhand: 1}}],
+  allLegs: [{name: '', stats:{legs: 1}}],
+  allHands: [{name: '', stats:{hands: 1}}],
+  allFeet: [{name: '', stats:{feet: 1}}],
+  allRings: [{name: '', stats:{ring: 1}}],
   loginModal: {
     open: false
   }
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'ADD_POST') {
-      return Object.assign({}, state, {
-        posts: state.posts.concat(action.payload)
-      })
-    }
-    if(action.type === 'ADD_WEAPON'){
-      return {
-        ...state,
-        allWeapons: state.allWeapons.concat(action.payload)
-      }
-    }
-  
-    if (action.type === 'LOAD_POSTS') {
-      return {
-        ...state,
-        posts: state.posts.concat(action.payload)
-      }
+    const {type, payload} = action;
+    switch(type){
+      case 'ADD_WEAPON': return{
+                            ...state,
+                            allWeapons: state.allWeapons.concat(payload)
+                          }
+      case 'ADD_HELM': return{
+                            ...state,
+                            allHelms: state.allHelms.concat(payload)
+                          }
+      case 'ADD_CAPE': return{
+                            ...state,
+                            allCapes: state.allCapes.concat(payload)
+                          }
+      case 'ADD_NECKS': return{
+                            ...state,
+                            allNecks: state.allNecks.concat(payload)
+                          }
+      case 'ADD_AMMO': return{
+                            ...state,
+                            allAmmo: state.allAmmo.concat(payload)
+                          }
+      case 'ADD_BODY': return{
+                            ...state,
+                            allBodies: state.allBodies.concat(payload)
+                          }
+      case 'ADD_OFFHAND': return{
+                            ...state,
+                            allOffhands: state.allOffhands.concat(payload)
+                          }
+      case 'ADD_LEGS': return{
+                            ...state,
+                            allLegs: state.allLegs.concat(payload)
+                          }
+      case 'ADD_HANDS': return{
+                            ...state,
+                            allHands: state.allHands.concat(payload)
+                          }  
+      case 'ADD_BOOTS': return{
+                            ...state,
+                            allFeet: state.allFeet.concat(payload)
+                          } 
+      case 'ADD_RING': return{
+                            ...state,
+                            allRings: state.allRings.concat(payload)
+                          }
+      case 'ADD_NECK': return{
+                            ...state,
+                            allNecks: state.allNecks.concat(payload)
+                          }
+      default: console.log('NO REDUCER MATCH:', payload);                                                                                                                                                         
+      
     }
     return state;
 }
