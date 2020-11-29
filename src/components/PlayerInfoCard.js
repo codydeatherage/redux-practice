1b6f3298b6b81cbf7e646225d9311f23d7a01a12
codydeatherage <cdeatherage14@gmail.com>
2020-11-28 23:53:37 -0800

    still working on ui

--- src/components/PlayerInfoCard.js
+++ src/components/PlayerInfoCard.js
@@ -8,48 +8,49 @@ class PlayerInfoCard extends Component{
     render(){
         return(
             <div className="card player-card">
-                <div className = "row row-1">
-                    <div className = "card-slot equip-slot" id="helm">
-                        <EquipmentDropDown></EquipmentDropDown>
+                <div className="player-equip-card">
+                    <div className = "row row-1">
+                        <div className = "card-slot equip-slot" id="helm">
+                            <EquipmentDropDown></EquipmentDropDown>
+                        </div>
+                    </div>
+                    <div className = "row">
+                        <div className = "card-slot equip-slot row-2" id="cape">
+                            <EquipmentDropDown></EquipmentDropDown>
+                        </div>
+                        <div className = "card-slot equip-slot row-2" id="neck">
+                            <EquipmentDropDown></EquipmentDropDown>
+                        </div>
+                        <div className = "card-slot equip-slot row-2" id="ammo">
+                            <EquipmentDropDown></EquipmentDropDown>
+                        </div>
+                    </div>
+                    <div className = "row">
+                        <div className = "card-slot equip-slot row-3" id="weapon">
+                            <EquipmentDropDown></EquipmentDropDown>
+                        </div>
+                        <div className = "card-slot equip-slot row-3" id="body">
+                            <EquipmentDropDown></EquipmentDropDown>
+                        </div>
+                        <div className = "card-slot equip-slot row-3" id="offhand">
+                            <EquipmentDropDown></EquipmentDropDown></div>                                                 
+                    </div>                
+                    <div className = "row">
+                        <div className = "card-slot equip-slot" id="legs">
+                            <EquipmentDropDown></EquipmentDropDown>
+                        </div>                    
+                    </div>                
+                    <div className = "row">
+                        <div className = "card-slot equip-slot row-3" id="gloves">
+                            <EquipmentDropDown></EquipmentDropDown>
+                        </div>
+                        <div className = "card-slot equip-slot row-3" id="boots">
+                            <EquipmentDropDown></EquipmentDropDown>
+                        </div>
+                        <div className = "card-slot equip-slot row-3" id="ring">
+                        <   EquipmentDropDown></EquipmentDropDown></div>                                                 
                     </div>
                 </div>
-                <div className = "row">
-                    <div className = "card-slot equip-slot row-2" id="cape">
-                        <EquipmentDropDown></EquipmentDropDown>
-                    </div>
-                    <div className = "card-slot equip-slot row-2" id="neck">
-                        <EquipmentDropDown></EquipmentDropDown>
-                    </div>
-                    <div className = "card-slot equip-slot row-2" id="ammo">
-                        <EquipmentDropDown></EquipmentDropDown>
-                    </div>
-                </div>
-                <div className = "row">
-                    <div className = "card-slot equip-slot row-3" id="weapon">
-                        <EquipmentDropDown></EquipmentDropDown>
-                    </div>
-                    <div className = "card-slot equip-slot row-3" id="body">
-                        <EquipmentDropDown></EquipmentDropDown>
-                    </div>
-                    <div className = "card-slot equip-slot row-3" id="offhand">
-                        <EquipmentDropDown></EquipmentDropDown></div>                                                 
-                </div>                
-                <div className = "row">
-                    <div className = "card-slot equip-slot" id="legs">
-                        <EquipmentDropDown></EquipmentDropDown>
-                    </div>                    
-                </div>                
-                <div className = "row">
-                    <div className = "card-slot equip-slot row-3" id="gloves">
-                        <EquipmentDropDown></EquipmentDropDown>
-                    </div>
-                    <div className = "card-slot equip-slot row-3" id="boots">
-                        <EquipmentDropDown></EquipmentDropDown>
-                    </div>
-                    <div className = "card-slot equip-slot row-3" id="ring">
-                    <   EquipmentDropDown></EquipmentDropDown></div>                                                 
-                </div>
-
             </div>
 
 
