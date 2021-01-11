58902ee54bb4bd91c7615481757f08eecf0dd89f
codydeatherage <cdeatherage14@gmail.com>
2021-01-10 19:34:10 -0800

    switched to lambda expressions to improve readability

--- src/components/PlayerSkillSlot.js
+++ src/components/PlayerSkillSlot.js
@@ -8,34 +8,23 @@ class PlayerSkillSlot extends Component{
     }
 
     handleChange(event){
-        console.log('ATK: ', event.target.value, '---');
+        if(event.target.value.length > 2){
+            event.target.value = event.target.value.substr(0, 2);
+        }
         const{atk, str, range, magic} = this.props.playerStats;
-        let payload = {};
-        if(event.target.value !== ''){
-            const input = parseInt(event.target.value);
-            switch(this.props.slot){
-                case 'atk_level' : payload = {atk: input, str: str, magic: magic, range: range}; break;
-                case 'str_level' : payload = {atk: atk, str: input, magic: magic, range: range}; break;                  
-                case 'magic_level' : payload = {atk: atk, str: str, magic: input, range: range}; break;
-                case 'ranged_level' : payload = {atk: atk, str: str, magic: magic, range: input}; break;
-                default: console.log('NO MATCH FOR PLAYER STAT');                                                       
-            }
+        let payload = {atk: atk, str: str, magic: magic, range: range};
+        const input = parseInt(event.target.value);
+        switch(this.props.slot){
+            case 'atk_level' : event.target.value !== '' ? payload.atk = input : payload.atk = 1; break;
+            case 'str_level' : event.target.value !== '' ? payload.str = input : payload.str = 1; break;                  
+            case 'magic_level' : event.target.value !== '' ? payload.magic = input : payload.magic = 1; break;
+            case 'ranged_level' : event.target.value !== '' ? payload.range = input : payload.range = 1; break;
+            default: console.log('Error modifying player stat ', this.props.slot);                                                       
         }
-        else{
-            switch(this.props.slot){
-                case 'atk_level' :  payload = {atk: 1, str: str, magic: magic, range: range}; break;
-                case 'str_level' : payload ={atk: atk, str: 1, magic: magic, range: range}; break;
-                case 'magic_level' : payload = {atk: atk, str: str, magic: 1, range: range}; break;
-                case 'ranged_level' : payload = {atk: atk, str: str, magic: magic, range: 1}; break;
-                default: console.log('NO MATCH FOR PLAYER STAT');                                                                        
-            }  
-        }
-        if(payload){
-            this.props.dispatch({
-                type: 'CHANGE_PLAYER_STAT',
-                payload: payload
-            });
-        }
+        this.props.dispatch({
+            type: 'CHANGE_PLAYER_STAT',
+            payload: payload
+        });    
     }
 
     render(){
@@ -43,7 +32,7 @@ class PlayerSkillSlot extends Component{
             <div className="form-input player-stat">
                 <div className="stat-icon my-auto" id={this.props.slot}></div>
                 <div className="player-stat-info">
-                    {this.props.bonuses.potion === 1? 
+                    {this.props.bonuses.potion === 1 ? 
                         <div className="boosted-stat">{this.props.value - 1}</div> 
                         :
                          <div className="boosted-stat">{this.props.value}</div>
