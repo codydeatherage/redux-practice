4960ddac0932b4b64f9893bd202e701fd9c89f2a
codydeatherage <cdeatherage14@gmail.com>
2021-03-08 00:21:55 -0800

    can now select AttackStyles

--- src/components/DataPanel/AttackStyleItem.js
+++ src/components/DataPanel/AttackStyleItem.js
@@ -1,18 +1,30 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 const AttackStyleItem = (props) => {
-    const [item, setSelection] = useState({selected: false})
-    const handleSelect = () =>{
-        setSelection({selected: !item.selected});
+    const [item, setSelection] = useState({ selected: false})
+   /*  const [img, setImage] = useState({ src: props.img }); */
+
+    const handleSelect = async () => {
+        let str = props.img.slice(0, props.img.indexOf('_crop')) + '_sel_crop.png';
+        
+        if(item.selected === true){
+            setSelection({ selected: false});
+        }else{
+            setSelection({ selected: true});
+        }
+       
     }
+
+   // let str = props.img.slice(0, props.img.indexOf('_crop')) + '_sel_crop.png';
+
     return (
-        <>  
-            {item.selected ? 
-                <img onClick={handleSelect} src={props.img} className="attack-style-img mr-0.5" alt=""></img>
-            :
-                <img onClick={handleSelect} src={props.img} className="attack-style-img mr-0.5" alt=""></img>
+        <>
+            {item.selected ?
+             <img onClick={handleSelect} src={props.img.slice(0, props.img.indexOf('_crop')) + '_sel_crop.png'} className="attack-style-img mr-0.5" alt=""></img>
+             :
+            <img onClick={handleSelect} src={props.img} className="attack-style-img mr-0.5" alt=""></img>
             }
-            
+
         </>
     )
 }
